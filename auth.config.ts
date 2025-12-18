4d7079d9f2ccabca02493e981d807df581a03311
gw.park <stripe2773@naver.com>
2025-12-18 13:57:24 +0900

    Update hero image to FICC version

--- auth.config.ts
+++ auth.config.ts
@@ -6,16 +6,20 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user; // Checks if a user is logged in by converting the presence of auth?.user to a boolean.
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
+      // 로그인 여부 상관없이 항상 통과시킵니다.
       return true;
     },
+    // authorized({ auth, request: { nextUrl } }) {
+    //   const isLoggedIn = !!auth?.user; // Checks if a user is logged in by converting the presence of auth?.user to a boolean.
+    //   const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+    //   if (isOnDashboard) {
+    //     if (isLoggedIn) return true;
+    //     return false; // Redirect unauthenticated users to login page
+    //   } else if (isLoggedIn) {
+    //     return Response.redirect(new URL('/dashboard', nextUrl));
+    //   }
+    //   return true;
+    // },
   },
   providers: [],
 } satisfies NextAuthConfig;
